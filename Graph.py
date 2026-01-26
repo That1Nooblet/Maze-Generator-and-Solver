4c2e8a9deb50d2455cdba1817c61ab6ebf8c10fd
That1Nooblet <brennan.t25@outlook.com>
2026-01-26 11:08:54 -0600

    Added the Code
    Graph Class and Maze Solver files
    Still need to implement:
    right clicking to select end tile
    display path length
    display currently selected algorithm

---
+++ Graph.py
@@ -0,0 +1,451 @@
+import random, sys, copy, os, pygame
+import io
+from collections import defaultdict
+from collections import deque
+from heapq import heappush, heappop, heapify
+
+class Graph():
+    def __init__(self, edges, weights = None, nvertices = 0, directed = False, clean = False):
+        self.nvertices = nvertices
+        self.edges = edges
+        self.directed = directed
+        if weights is None:
+            self.weights = {}
+        else:
+            self.weights = weights
+
+        if clean:
+            self.cleanup()
+        self.update_undirected_edges()
+    
+    def update_undirected_edges(self):
+        if not self.directed:
+            # make updated adjacency list
+            newEdges = defaultdict(set)
+            for a, paths in self.edges.items():
+                newEdges[a].update(paths)
+                for b in paths:
+                    newEdges[b].add(a)
+            
+            # make updated weights dict
+            newWeights = {}
+            for (a,b), w in self.weights.items():
+                newWeights[(a,b)] = w
+                newWeights[(b,a)] = w
+            
+            self.edges = newEdges
+            self.weights = newWeights
+
+    def cleanup(self):
+        newV = {}
+        vertices = set() # keeps track of all old vertices
+        for v, paths in self.edges.items():
+            vertices.add(v)
+            vertices.update(paths)
+
+        # pairs old vertices with new number, in sorted order because set
+        for i, v in enumerate(vertices):
+            newV[v] = i
+        
+        # make new cleaned up adjacency list
+        newEdges = defaultdict(set)
+        for a, paths in self.edges.items():
+            newA = newV[a]
+            for b in paths:
+                newB = newV[b]
+                newEdges[newA].add(newB)
+
+        # make new cleaned up weights dict
+        newWeights = {}
+        for (a,b), w in self.weights.items():
+            newA, newB = newV[a], newV[b]
+            newWeights[(newA, newB)] = w
+
+        self.edges = newEdges
+        self.weights = newWeights
+    
+    # helper for the file reading in the from_file that gets rid of comments and leading/trailing whitespace
+    @staticmethod
+    def stripLine(line):
+        for i in range(len(line)):
+            if line[i] == '#':
+                line = line[:i]
+                break
+        line = line.strip()
+        return line
+
+    @classmethod
+    def from_file(cls, openfile, directed = False):
+        f = openfile.readlines()
+        weightLine = len(f) # default value so if not found it won't look for weights
+
+        # find special comment "# WEIGHTS" and strip comments from other lines
+        for i in range(len(f)):
+            if "# WEIGHTS" in f[i]:
+                weightLine = i
+            
+            f[i] = Graph.stripLine(f[i])
+        
+        # make adjacency lists and keep track of all the vertices
+        vertices = set()
+        edges = defaultdict(set)
+        for i in range(weightLine):
+            if f[i] == "":
+                continue
+            
+            adjList = list(map(int, f[i].split(" ")))
+            edges[adjList[0]] = set(adjList[1:])
+            vertices.update(adjList)
+
+        nvertices = len(vertices)
+
+        # initialize weights if they exist
+        weights = {}
+        for i in range(weightLine+1, len(f)):
+            if f[i] == "":
+                continue
+
+            a, b, w = map(int, f[i].split(" "))
+            weights[(a,b)] = w
+
+        return Graph(edges, weights, nvertices, directed)
+
+    @classmethod
+    def from_str(cls, txt, directed = False):
+        openfile = io.StringIO(txt)
+        
+        graph = Graph.from_file(openfile, directed)
+        openfile.close()
+
+        return graph
+    
+    def dfs_path(self, start, end):
+        vis = [False] * self.nvertices
+        disc = [False] * self.nvertices
+        prev = [-1] * self.nvertices
+        order = []
+
+        def dfs(v):
+            disc[v] = True
+            order.append((v, 0))
+            for dest in self.edges[v]:
+                if not disc[dest]:
+                    prev[dest] = v
+                    dfs(dest)
+                if (vis[end]): return
+            vis[v] = True
+            order.append((v, 1))
+        
+        dfs(start)
+
+        path = []
+        if vis[end]:
+            v = end
+            while (v != -1):
+                path.append(v)
+                v = prev[v]
+            
+            path.reverse()
+            
+        return order, path
+    
+    def bfs_path(self, start, end):
+        vis = [False] * self.nvertices
+        disc = [False] * self.nvertices
+        prev = [-1] * self.nvertices
+        order = []
+
+        def bfs(start, end):
+            q = deque()
+            q.append(start)
+            disc[start] = True
+            order.append((start, 0))
+
+            while q and not vis[end]:
+                cur = q.popleft()
+                assert not vis[cur]
+
+                for dest in self.edges[cur]:
+                    if not disc[dest]:
+                        disc[dest] = True
+                        prev[dest] = cur
+                        order.append((dest, 0))
+                        q.append(dest)
+
+                vis[cur] = True
+                order.append((cur, 1))
+        
+        bfs(start, end)
+
+        path = []
+        if vis[end]:
+            v = end
+            while (v != -1):
+                path.append(v)
+                v = prev[v]
+            
+            path.reverse()
+            
+        return order, path
+    
+    def bfs2way_path(self, start, end):
+        vis = [False] * self.nvertices
+        disc = [False] * self.nvertices
+        prev = [-1] * self.nvertices
+        goal = [-1] * self.nvertices
+        order = []
+
+        def bfs2way(start, end):
+            q = deque()
+            q.append(start)
+            q.append(end)
+            disc[start] = True
+            disc[end] = True
+            goal[start] = end
+            goal[end] = start
+            order.append((start, 0))
+            order.append((end, 0))
+
+            while q:
+                cur = q.popleft()
+                assert not vis[cur]
+
+                for dest in self.edges[cur]:
+                    if vis[dest] and goal[dest] != goal[cur]:
+                        vis[cur] = True
+                        order.append((cur, 1))
+                        if goal[cur] == end: return cur, dest
+                        else: return dest, cur
+                        
+                    if not disc[dest]:
+                        disc[dest] = True
+                        prev[dest] = cur
+                        goal[dest] = goal[cur]
+                        order.append((dest, 0))
+                        q.append(dest)
+
+                vis[cur] = True
+                order.append((cur, 1))
+
+            return -1, -1
+        
+        p1, p2 = bfs2way(start, end)
+
+        path = []
+        if p1 != -1:
+            v = p1
+            while (v != -1):
+                path.append(v)
+                v = prev[v]
+            
+            path.reverse()
+
+            v = p2
+            while (v != -1):
+                path.append(v)
+                v = prev[v]
+            
+        return order, path
+
+    def astar_path(self, start, end, heuristic, weight = 1):
+        vis = [False] * self.nvertices
+        disc = [False] * self.nvertices
+        dist = [0] * self.nvertices
+        prev = [-1] * self.nvertices
+        order = []
+        path = []
+
+        pq = []
+        # cost, heuristic, moves, node
+        startHeur = heuristic(start, end)
+        heappush(pq, (weight * startHeur, startHeur, 0, start))
+        disc[start] = True
+        order.append((start, 0))
+        
+        while pq:
+            curCost, curHeur, moves, cur = heappop(pq)
+            if dist[cur] < moves or vis[cur]:
+                continue
+
+            if cur == end:
+                vis[cur] = True
+                order.append((cur, 1))
+                break
+
+            for nbr in self.edges[cur]:
+                if vis[nbr]: continue
+                newHeur = heuristic(nbr, end)
+                newCost = weight * newHeur + moves + 1
+                if not disc[nbr] or dist[nbr] > moves + 1:
+                    dist[nbr] = moves + 1
+                    disc[nbr] = True
+                    order.append((nbr, 0))
+                    prev[nbr] = cur
+                    heappush(pq, (newCost, newHeur, moves + 1, nbr))
+
+            vis[cur] = True
+            order.append((cur, 1))
+        
+        if vis[end]:
+            v = end
+            
+            while (v != -1):
+                path.append(v)
+                v = prev[v]
+            
+            path.reverse()
+
+        return order, path
+    
+    def astar2way_path(self, start, end, heuristic, weight=1):
+        n = self.nvertices
+
+        # forward search
+        vis_f  = [False] * n
+        disc_f = [False] * n
+        dist_f = [0] * n
+        prev_f = [-1] * n
+
+        # backward search
+        vis_b  = [False] * n
+        disc_b = [False] * n
+        dist_b = [0] * n
+        prev_b = [-1] * n
+
+        pq_f = []
+        pq_b = []
+
+        order = []
+
+        # initialize
+        startHeur = heuristic(start, end)
+        endHeur   = heuristic(end, start)
+
+        heappush(pq_f, (weight * startHeur, startHeur, 0, start))
+        heappush(pq_b, (weight * endHeur,   endHeur,   0, end))
+
+        disc_f[start] = True
+        disc_b[end]   = True
+
+        order.append((start, 0))
+        order.append((end, 0))
+
+        meet = -1
+
+        while pq_f and pq_b:
+            # ---------- FORWARD STEP ----------
+            cost, heur, moves, cur = heappop(pq_f)
+            if dist_f[cur] < moves or vis_f[cur]:
+                pass
+            else:
+                for nbr in self.edges[cur]:
+                    if vis_f[nbr]:
+                        continue
+                    if disc_b[nbr]:
+                        meet = nbr
+                        prev_f[nbr] = cur
+                        break
+
+                    newHeur = heuristic(nbr, end)
+                    newCost = weight * newHeur + moves + 1
+
+                    if not disc_f[nbr] or dist_f[nbr] > moves + 1:
+                        dist_f[nbr] = moves + 1
+                        disc_f[nbr] = True
+                        prev_f[nbr] = cur
+                        order.append((nbr, 0))
+                        heappush(pq_f, (newCost, newHeur, moves + 1, nbr))
+
+                vis_f[cur] = True
+                order.append((cur, 1))
+
+            if meet != -1:
+                break
+
+            # ---------- BACKWARD STEP ----------
+            cost, heur, moves, cur = heappop(pq_b)
+            if dist_b[cur] < moves or vis_b[cur]:
+                continue
+
+            for nbr in self.edges[cur]:
+                if vis_b[nbr]:
+                    continue
+                if disc_f[nbr]:
+                    meet = nbr
+                    prev_b[nbr] = cur
+                    break
+
+                newHeur = heuristic(nbr, start)
+                newCost = weight * newHeur + moves + 1
+
+                if not disc_b[nbr] or dist_b[nbr] > moves + 1:
+                    dist_b[nbr] = moves + 1
+                    disc_b[nbr] = True
+                    prev_b[nbr] = cur
+                    order.append((nbr, 0))
+                    heappush(pq_b, (newCost, newHeur, moves + 1, nbr))
+
+            vis_b[cur] = True
+            order.append((cur, 1))
+
+            if meet != -1:
+                break
+
+        # ---------- PATH RECONSTRUCTION ----------
+        path = []
+        if meet != -1:
+            v = meet
+            while v != -1:
+                path.append(v)
+                v = prev_f[v]
+            path.reverse()
+
+            v = prev_b[meet]
+            while v != -1:
+                path.append(v)
+                v = prev_b[v]
+
+        return order, path
+
+    # count how many connected components in the graph
+    def connected_components(self):
+        vis = [False] * self.nvertices
+
+        def dfs(v):
+            assert vis[v] == False
+            vis[v] = True
+            for dest in self.edges[v]:
+                if not vis[dest]:
+                    dfs(dest)
+        
+        cnt = 0
+        for v in range(self.nvertices):
+            if not vis[v]:
+                dfs(v)
+                cnt += 1
+        
+        return cnt
+
+    def topo_sort(self):
+        vis = [False] * self.nvertices
+        order = []
+
+        def dfs(v):
+            vis[v] = True
+            
+            for adj in self.edges[v]:
+                if not vis[adj]:
+                    dfs(adj)
+            
+            order.append(v)
+
+        for v in range(self.nvertices):
+            if not vis[v]:
+                dfs(v)
+        
+        order.reverse()
+
+        return order
+
+    def __repr__(self):
+        return f"[Graph, V={self.nvertices}, E={dict(self.edges)}, W={self.weights}]"
---
+++ Maze Solver.py
@@ -0,0 +1,353 @@
+import Graph
+import random, sys, copy, os, pygame
+from collections import defaultdict
+import pygame
+from pygame.locals import *
+
+# ----------MAZE MAKER----------
+
+QUIT = (-100,-100)
+gridSize = (50,50)
+tileSize = 11
+halfSize = tileSize // 2
+size = (gridSize[0] * tileSize, gridSize[1] * tileSize)
+
+def init():
+    global screen, clock
+    sys.setrecursionlimit(10000)
+    clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("maze thing")
+
+def main():
+    init()
+    
+    running = True
+    model = {
+            "state" : "build", 
+            "start" : (-1,-1),
+            "end" : (gridSize[0] - 1, gridSize[1] - 1), 
+            "search" : "astar",
+            "order_index" : -1, 
+            "order" : [], 
+            "path" : [], 
+            "grid" : defaultdict(list)
+            }
+    
+    while(running):
+        # increment path animation every tick
+        if (model["state"] == "graph"):
+            model["order_index"] = min(model["order_index"] + 1, len(model["order"]) + len(model["path"]))
+
+        # removing the negative values (the hover values) from the dict
+        for pos, dirs in model["grid"].items():
+            for i, d in enumerate(dirs):
+                if d < 0:
+                    dirs.pop(i)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or model == QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                model = key_handler(model, event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                model = mouse_handler(model, event)
+        
+        draw_handler(model)
+        pygame.display.flip()
+        clock.tick(1000)
+
+def getWallDir(dx, dy):
+    dists = [dy, tileSize-dx, tileSize-dy, dx]
+    minIdx = 0
+    minDist = dy
+    for i in range(4):
+        if (dists[i] < minDist):
+                minIdx = i
+                minDist = dists[i]
+    return minIdx
+
+def draw_handler(model):
+    global screen
+    bgcolor = pygame.color.Color("orange")
+    screen.fill(bgcolor)
+    
+    wallDir = {0 : (0,-1), 1 : (1,0), 2 : (0,1), 3 : (-1, 0)}
+    
+    # color the nodes in the traversal order
+    if model["state"] == "graph" and model["order_index"] != -1:
+        visColor = pygame.color.Color("blue")
+        discColor = pygame.color.Color("aqua")
+        idx = model["order_index"]
+        order = model["order"]
+
+        for i in range(min(idx+1, len(order))):
+            curNode, nodeState = order[i]
+            gridX, gridY = nodeToGrid(curNode)
+            x, y = gridX * tileSize, gridY * tileSize
+            square = pygame.Rect(x, y, tileSize, tileSize)
+            if (nodeState == 1): pygame.draw.rect(screen, visColor, square)
+            if (nodeState == 0): pygame.draw.rect(screen, discColor, square)
+        
+        if idx > len(order):
+            path = model["path"]
+            pathIdx = min(idx - len(order), len(path))
+            pathColor = pygame.color.Color("green")
+            for i in range(pathIdx):
+                curNode = path[i]
+                gridX, gridY = nodeToGrid(curNode)
+                x, y = gridX * tileSize, gridY * tileSize
+                square = pygame.Rect(x, y, tileSize, tileSize)
+                pygame.draw.rect(screen, pathColor, square)
+    
+    # get mouse grid pos and closest wall and draw hover position during "build" state
+    if model["state"] == "build":
+        mx, my = pygame.mouse.get_pos()
+        mdx, mdy = mx % tileSize, my % tileSize
+        mouseDir = getWallDir(mdx, mdy)
+        mgx, mgy= mx // tileSize, my // tileSize
+        mx, my = mgx * tileSize + halfSize, mgy * tileSize + halfSize
+        
+        wall = wallDir[mouseDir]
+        mStart = pygame.math.Vector2(mx + wall[0] * halfSize, my + wall[1] * halfSize)
+        mEnd = pygame.math.Vector2(mx + wall[0] * halfSize, my + wall[1] * halfSize)
+        if wall[0] == 0:
+            mStart[0] += halfSize
+            mEnd[0] += -halfSize
+        if wall[1] == 0:
+            mStart[1] += halfSize
+            mEnd[1] += -halfSize
+            
+        if mouseDir in model["grid"][(mgx,mgy)]:
+                pygame.draw.line(screen, "red", mStart, mEnd)
+        else:
+                pygame.draw.line(screen, "blue", mStart, mEnd)
+                
+    # draw walls
+    wall_color = pygame.color.Color("black")
+    
+    for pos, dirs in model["grid"].items():
+        (gridX, gridY) = pos
+        x, y = gridX * tileSize + halfSize, gridY * tileSize + halfSize
+        for d in dirs:
+            wall = wallDir[d]
+            start = pygame.math.Vector2(x + wall[0] * halfSize, y + wall[1] * halfSize)
+            end = pygame.math.Vector2(x + wall[0] * halfSize, y + wall[1] * halfSize)
+            if wall[0] == 0:
+                start[0] += halfSize
+                end[0] += -halfSize
+            if wall[1] == 0:
+                start[1] += halfSize
+                end[1] += -halfSize
+                    
+            start = pygame.math.Vector2(start)
+            end = pygame.math.Vector2(end)
+            pygame.draw.line(screen, wall_color, start, end)
+
+def inBound(gridPos):
+    x, y = gridPos
+    xBound = 0 <= x and x < gridSize[0]
+    yBound = 0 <= y and y < gridSize[1]
+    return xBound and yBound
+
+def reset(model):
+    model["grid"] = defaultdict(list)
+    model["state"] = "build"
+    model["order_index"] = -1
+    model["order"] = []
+    model["path"] = []
+
+def genMaze1(model):
+    wallDir = {0 : (0,-1), 1 : (1,0), 2 : (0,1), 3 : (-1, 0)}
+    reset(model)
+    model["state"] = "graph"
+    for x in range(gridSize[0]):
+        for y in range(gridSize[1]):
+            for dir in range(4):
+                if dir in model["grid"][(x, y)]:
+                    continue
+                pairPos = (x + wallDir[dir][0], y + wallDir[dir][1])
+                pairDir = (dir + 2) % 4
+                if not inBound(pairPos):
+                    continue
+                makeWall = random.randint(1,4)
+                if makeWall == 4:
+                    model["grid"][(x, y)].append(dir)
+                    model["grid"][pairPos].append(pairDir)
+
+def break_wall(model, x, y, d):
+    DIRS = [(0,-1),(1,0),(0,1),(-1,0)]
+    OPP = {0:2, 1:3, 2:0, 3:1}
+
+    nx = x + DIRS[d][0]
+    ny = y + DIRS[d][1]
+
+    if not inBound((nx, ny)):
+        return
+
+    if d in model["grid"][(x, y)]:
+        model["grid"][(x, y)].remove(d)
+    if OPP[d] in model["grid"][(nx, ny)]:
+        model["grid"][(nx, ny)].remove(OPP[d])
+
+def genMaze2(model, break_wall_prob=0.05):
+    reset(model)
+    model["state"] = "graph"
+
+    DIRS = [(0,-1),(1,0),(0,1),(-1,0)]
+    OPP = {0:2, 1:3, 2:0, 3:1}
+
+    w, h = gridSize
+    visited = [[False]*h for _ in range(w)]
+
+    # start with all walls
+    for x in range(w):
+        for y in range(h):
+            model["grid"][(x,y)] = [0,1,2,3]
+
+    def dfs(x, y):
+        visited[x][y] = True
+        dirs = [0,1,2,3]
+        random.shuffle(dirs)
+
+        for d in dirs:
+            nx = x + DIRS[d][0]
+            ny = y + DIRS[d][1]
+
+            if not inBound((nx,ny)) or visited[nx][ny]:
+                continue
+
+            break_wall(model, x, y, d)
+            dfs(nx, ny)
+
+    # build perfect maze
+    dfs(0, 0)
+
+    # ---------- ADD MULTIPLE PATHS ----------
+    for x in range(w):
+        for y in range(h):
+            for d in range(4):
+                if d in model["grid"][(x,y)]:
+                    if random.random() < break_wall_prob:
+                        break_wall(model, x, y, d)
+
+        
+def key_handler(model, event):
+    if len(model) == 0:
+        return model
+    
+    k = event.key
+    
+    if k == pygame.K_q:
+        model = QUIT
+    elif k == pygame.K_a or k == pygame.K_LEFT:
+        model["order_index"] = max(model["order_index"] - 1, 0)
+    elif k == pygame.K_d or k == pygame.K_RIGHT:
+        model["order_index"] = min(model["order_index"] + 1, len(model["order"]) + len(model["path"]) - 1)
+    elif k == pygame.K_r:
+        reset(model)
+    elif k == pygame.K_g:
+        genMaze1(model)
+    elif k == pygame.K_h:
+        genMaze2(model)
+    elif k == pygame.K_SPACE and model["state"] == "build":
+        model["state"] = "graph"
+        model["order_index"] = -1
+        model["order"] = []
+        model["path"] = []
+    elif k == pygame.K_SPACE and model["state"] == "graph":
+        model["state"] = "build"
+    elif k == pygame.K_p:
+        model["order_index"] = len(model["order"]) - 1
+    elif k == pygame.K_1:
+        model["search"] = "dfs"
+    elif k == pygame.K_2:
+        model["search"] = "bfs"
+    elif k == pygame.K_3:
+        model["search"] = "astar"
+    elif k == pygame.K_4:
+        model["search"] = "astar_weighted"
+    elif k == pygame.K_5:
+        model["search"] = "bfs2way"
+    elif k == pygame.K_6:
+        model["search"] = "astar2way"
+            
+    return model
+
+def gridToNode(pos):
+    x, y = pos
+    node = y * gridSize[0] + x
+
+    return node
+
+def nodeToGrid(node):
+    x = node % gridSize[0]
+    y = node // gridSize[0]
+
+    return (x,y)
+
+def heuristic(node, goal):
+    nodeX, nodeY = nodeToGrid(node)
+    goalX, goalY = nodeToGrid(goal)
+
+    return abs(nodeX - goalX) + abs(nodeY - goalY)
+    
+def mouse_handler(model, event):
+    x, y = event.pos
+    dx = x % tileSize
+    dy = y % tileSize
+    
+    # find out which wall the click was closest to
+    minIdx = getWallDir(dx, dy)
+    wallDir = {0 : (0,-1), 1 : (1,0), 2 : (0,1), 3 : (-1, 0)}
+    wall = wallDir[minIdx]
+
+    gridX = x // tileSize
+    gridY = y // tileSize
+    gridPos = (gridX, gridY)
+    pairPos = (gridX + wall[0], gridY + wall[1])
+    pairDir = (minIdx + 2) % 4
+    if event.type == pygame.MOUSEBUTTONDOWN and model["state"] == "build":
+        # remove the wall if its already there and add it if not
+        if minIdx in model["grid"][gridPos]:
+            for i, d in enumerate(model["grid"][gridPos]):
+                if minIdx == d:
+                    model["grid"][gridPos].pop(i)
+            for i, d in enumerate(model["grid"][pairPos]):
+                if pairDir == d:
+                    model["grid"][pairPos].pop(i)
+        else:
+            model["grid"][gridPos].append(minIdx)
+            model["grid"][pairPos].append(pairDir)
+        
+    elif event.type == pygame.MOUSEBUTTONDOWN and model["state"] == "graph":
+        model["order_index"] = 0
+        edges = defaultdict(list)
+        nvertices = gridSize[0] * gridSize[1]
+        for node in range(nvertices):
+            curGridPos = nodeToGrid(node)
+            for dir in range(4):
+                if dir not in model["grid"][curGridPos]:
+                    shift = wallDir[dir]
+                    neighborGrid = (curGridPos[0] + shift[0], curGridPos[1] + shift[1])
+                    if inBound(neighborGrid):
+                        neighborNode = gridToNode(neighborGrid)
+                        edges[node].append(neighborNode)
+        
+        graph = Graph.Graph(edges, nvertices = nvertices)
+        start = gridToNode(gridPos)
+        end = gridToNode((gridSize[0] - 1, gridSize[1] - 1))
+        if model["search"] == "dfs": model["order"], model["path"] = graph.dfs_path(start, end)
+        if model["search"] == "bfs": model["order"], model["path"] = graph.bfs_path(start, end)
+        if model["search"] == "astar": model["order"], model["path"] = graph.astar_path(start, end, heuristic)
+        if model["search"] == "astar_weighted": model["order"], model["path"] = graph.astar_path(start, end, heuristic, weight = 100)
+        if model["search"] == "bfs2way": model["order"], model["path"] = graph.bfs2way_path(start, end)
+        if model["search"] == "astar2way": model["order"], model["path"] = graph.astar2way_path(start, end, heuristic)
+        print(len(model["path"]))
+
+    return model
+
+        
+if __name__ == '__main__':
+    main()
