ef44bb1ca5a3797776d7baa031e0a46e4adf54d0
That1Nooblet <brennan.t25@outlook.com>
2026-01-27 09:38:07 -0600

    test2 gitignore

--- Maze Solver.py
+++ Maze Solver.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 # ----------MAZE MAKER----------
 
 QUIT = "EXIT"
-gridSize = (50, 150)
+gridSize = (50, 50)
 screenHeight = 600
 tileSize = screenHeight // gridSize[1]
 halfSize = tileSize // 2
